b2dff691fdd5ebb4e59186fb533dff4f915381f5
koushikAhmedsHuvo <131914978+koushikAhmedsHuvo@users.noreply.github.com>
2024-04-01 20:56:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function searchCountry() {
+  const searchInput = document.getElementById("searchInput").value;
+  fetch(`https://restcountries.com/v3.1/name/${searchInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length > 0) {
+        const country = data[0];
+        const countryInfo = `
+          <h2>${country.name.common}</h2>
+          <p>Capital: ${country.capital}</p>
+          <p>Population: ${country.population}</p>
+          <p>Region: ${country.region}</p>
+          <button onclick="showWeather('${country.name.common}')">Show Weather</button>
+        `;
+        document.getElementById("countryInfo").innerHTML = countryInfo;
+      } else {
+        document.getElementById("countryInfo").innerHTML =
+          "<p>Country not found</p>";
+      }
+    })
+    .catch((error) => console.log("Error:", error));
+}
+
+function showWeather(countryName) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=15d62855a8cde5311ed5b82e995e2fd3`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherInfo = `
+        <p>Temperature: ${Math.round(data.main.temp - 273.15)}°C</p>
+        <p>Description: ${data.weather[0].description}</p>
+      `;
+      document
+        .getElementById("countryInfo")
+        .insertAdjacentHTML("beforeend", weatherInfo);
+    })
+    .catch((error) => console.log("Error:", error));
+}
